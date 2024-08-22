c130b7f87ad6ab9b2d22a929bd06af1e3d6c2b13
Totti Rdz <thorsten_herfurtner@gmx.de>
2024-08-22 19:20:39 +0200

    feat: translate description on home page to german

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,19 +11,19 @@ export default function HomePage() {
       <section className="py-16 md:py-24 lg:py-32">
         <div className="container flex max-w-screen-md flex-col items-center gap-4 text-center">
           <h1 className="!leading-none font-bold text-3xl sm:text-5xl md:text-6xl lg:text-7xl landing-header py-2">
-            Share <strong>Expenses</strong> <br /> with <strong>Friends</strong>{' '}
-            & <strong>Family</strong>
+            Teilt <strong>Ausgaben</strong> <br /> mit <strong>Freunden</strong>{' '}
+            & <strong>Familie</strong>
           </h1>
           <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-            Welcome to your new <strong>Spliit</strong> instance! <br />
-            Customize this page by editing <em>src/app/page.tsx</em>.
+            Erstellt Gruppen und seht, wer wem wie viel schuldet.
+            <br />
           </p>
           <div className="flex gap-2">
             <Button asChild>
-              <Link href="/groups">Go to groups</Link>
+              <Link href="/groups">Loslegen</Link>
             </Button>
             <Button asChild variant="secondary">
-              <Link href="https://github.com/spliit-app/spliit">
+              <Link href="https://github.com/totti-rdz/spliit">
                 <Github className="w-4 h-4 mr-2" />
                 GitHub
               </Link>
